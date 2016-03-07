63630b5a15934fc687b70afeac9a49813dd00445
pragyapandey31 <pragya.pandey15@gmail.com>
2016-03-07 15:16:56 +0530

    Create Welcome.java

---
+++ Java-Problems/Introduction/WelcomToJava/Welcome.java
@@ -0,0 +1,9 @@
+//Write your code here.
+//Name of your public class should be 'Solution'.
+class Welcome
+    {
+    public static void main(String args[])
+        {
+        System.out.println("Hello World."+"\nHello Java.");
+    }
+}

36c8ad5a42cf5b9b6801571e6c243a0dddd63a52
pragyapandey31 <pragya.pandey15@gmail.com>
2016-07-06 20:06:32 +0530

    Create Kangaroo.java

---
+++ algorithms/Implementation/Kangaroo.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Kangaroo{
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        if(v2>v1)
+            System.out.println("NO");
+        else 
+            {
+         while(x2>x1)
+             {
+             x1=x1+v1;
+             x2=x2+v2;
+         if(x1==x2)
+             {   System.out.println("YES");
+        }
+         }
+            if(x1>x2)
+            System.out.println("NO");
+        
+        }
+    }
+}
